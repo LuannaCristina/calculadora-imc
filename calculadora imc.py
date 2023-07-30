c21591da5c0c137f3a2ddc92cd06f1a21e959ec1
Luanna Cristina <luannaandrade95@gmail.com>
2023-07-30 09:58:03 -0300

    primeiro commit

---
+++ calculadora imc.py
@@ -0,0 +1,16 @@
+nome = str (input( 'Ola, tudo bem? Qual seu nome? '))
+peso = int (input( 'Quantos quilos você tem? : '))
+altura = float (input( 'E qual a sua altura? : '))
+
+imc = peso / altura **2
+
+print (nome ,'Seu IMC é: ', (imc) )
+
+
+
+
+
+
+
+
+
